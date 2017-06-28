0689e3ec700f47e0bf9ad762e062fb84bbe98d4d
POM <pom7@bk.ru>
2017-06-28 19:48:53 +0300

    Added containair

---
+++ src/main/java/chapter02/LoggingInterceptor.java
@@ -0,0 +1,40 @@
+package chapter02;
+
+import javax.inject.Inject;
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.Interceptor;
+import javax.interceptor.InvocationContext;
+import java.util.logging.Logger;
+
+/**
+ * @author Antonio Goncalves
+ *         APress Book - Beginning Java EE 7 with Glassfish 4
+ *         http://www.apress.com/
+ *         http://www.antoniogoncalves.org
+ *         --
+ */
+@Interceptor
+@Loggable
+public class LoggingInterceptor {
+
+    // ======================================
+    // =             Attributes             =
+    // ======================================
+
+    @Inject
+    private Logger logger;
+
+    // ======================================
+    // =          Business methods          =
+    // ======================================
+
+    @AroundInvoke
+    public Object logMethod(InvocationContext ic) throws Exception {
+        logger.entering(ic.getTarget().getClass().getName(), ic.getMethod().getName());
+        try {
+            return ic.proceed();
+        } finally {
+            logger.exiting(ic.getTarget().getClass().getName(), ic.getMethod().getName());
+        }
+    }
+}
---
+++ src/main/java/chapter02/ex01/NumberGenerator01.java
@@ -0,0 +1,18 @@
+package chapter02.ex01;
+
+
+/**
+ * @author Antonio Goncalves
+ *         APress Book - Beginning Java EE 7 with Glassfish 4
+ *         http://www.apress.com/
+ *         http://www.antoniogoncalves.org
+ *         --
+ */
+public interface NumberGenerator01 {
+
+  // ======================================
+  // =          Business methods          =
+  // ======================================
+
+  String generateNumber();
+}
