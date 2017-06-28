793707b2d7f4222d080605c4b0b0305caaa6f865
POM <pom7@bk.ru>
2017-06-28 19:51:52 +0300

    Added containair

---
+++ src/main/java/chapter02/ex02/NumberGenerator02.java
@@ -0,0 +1,18 @@
+package chapter02.ex02;
+
+
+/**
+ * @author Antonio Goncalves
+ *         APress Book - Beginning Java EE 7 with Glassfish 4
+ *         http://www.apress.com/
+ *         http://www.antoniogoncalves.org
+ *         --
+ */
+public interface NumberGenerator02 {
+
+  // ======================================
+  // =          Business methods          =
+  // ======================================
+
+  String generateNumber();
+}
