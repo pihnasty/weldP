c09d32fb77deeae8baa8f32ba477cd30486a8424
POM <pom7@bk.ru>
2017-07-04 22:47:31 +0300

    added ex01 (Here was exception)

---
+++ src/test/java/charter02/ex01/Book01Test.java
@@ -0,0 +1,35 @@
+package charter02.ex01;
+
+
+import charter02.ex02.Book02;
+import charter02.ex02.BookService02;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author Antonio Goncalves
+ *         APress Book - Beginning Java EE 7 with Glassfish 4
+ *         http://www.apress.com/
+ *         http://www.antoniogoncalves.org
+ *         --
+ */
+public class Book01Test {
+
+  // ======================================
+  // =              Methods               =
+  // ======================================
+
+  @Test
+  public void shouldCheckNumberIsThirteenDigits() {
+
+    BookService02 bookService = new BookService02();
+
+    Book02 book = bookService.createBook("H2G2", 12.5f, "Geeky scifi Book");
+
+    System.out.println(book.toString());
+
+
+    assertTrue(book.getIsbn().startsWith("13"));
+  }
+}
