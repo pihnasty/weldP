d50633b2680a9e6aaf0057508954ca92b811ceb8
POM <pom7@bk.ru>
2017-07-04 22:42:49 +0300

    added ex02

---
+++ src/test/java/charter02/ex02/NumberGenerator02Test.java
@@ -0,0 +1,29 @@
+package charter02.ex02;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author Antonio Goncalves
+ *         APress Book - Beginning Java EE 7 with Glassfish 4
+ *         http://www.apress.com/
+ *         http://www.antoniogoncalves.org
+ *         --
+ */
+public class NumberGenerator02Test {
+
+  // ======================================
+  // =              Methods               =
+  // ======================================
+
+  @Test
+  public void shouldCheckNumberIsThirteenDigits() {
+
+    BookService02 bookService = new BookService02();
+
+    Book02 book = bookService.createBook("H2G2", 12.5f, "Geeky scifi Book");
+    System.out.println(book.getIsbn());
+    assertTrue(book.getIsbn().startsWith("13"));
+  }
+}
